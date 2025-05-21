c2a73fecb6fce2cacfa49453044f5cd828b84afd
Shaozzz123 <145885415+Shaozzz123@users.noreply.github.com>
2025-05-21 16:14:41 +0800

    Update trational_methon.py

--- trational_methon.py
+++ trational_methon.py
@@ -1,12 +1,12 @@
 import pycorrector 
+from pycorrector import Corrector
 
-# 待纠正文本（包含错别字）
-text = "少先队员因该为老人让坐"
+m = Corrector()
+text = input("请输入待纠错文本：")
 
-# 调用 pycorrector 进行纠错
-corrected_text, detail = pycorrector.correct(text)
+corrected_text = m.correct(text) 
 
-# 输出结果
-print("原句：", text)
-print("纠正后：", corrected_text)
-print("错误详情：", detail)  # [(错字, 正确字, 位置)]
+print("\n原始文本：", text)
+print("\n")
+print("纠正后文本：", corrected_text)
+
